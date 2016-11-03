e53bb68a6f7aa998eb40d75c09c46a6291f4b710
Jayasurya-Seetharaman <jayasuryas06@gmail.com>
2016-11-03 07:16:41 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  var x=0;
-do{
-    alert("Hello");
-}while(x!=5);
-
 });
 
 app.get('/ui/style.css', function (req, res) {
