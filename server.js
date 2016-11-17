f9dcdcd59710b0daf95cb9980054cbbcd08fe165
Jayasurya-Seetharaman <jayasuryas06@gmail.com>
2016-11-17 09:38:13 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/ui/main.js', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter + 1;    
-  res.send(counter.toString());
+  alert("hello");
   console.log(counter);
 });
 
