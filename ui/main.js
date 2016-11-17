dd7a548705135904c50591a23b55f34f1c5124ef
Jayasurya-Seetharaman <jayasuryas06@gmail.com>
2016-11-17 09:30:26 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
 console.log('Loaded!');
 
 var count = 10;
